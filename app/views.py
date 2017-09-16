31fb7fd95f8ef976e29b8c7db42e37399b6b2f9b
Ben Coleman <benc.uk@gmail.com>
2017-09-16 18:23:52 +0100

    Added realtime monitoring

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
-from flask import url_for, redirect, render_template, flash, g, session
+from flask import url_for, redirect, render_template, flash, g, session, jsonify
 from app import app
-#import socket
 import cpuinfo
 import psutil
 import platform
@@ -12,10 +11,41 @@ def index():
 @app.route('/info')
 def info():
    osinfo = {}
-   #osinfo['hostname'] = socket.gethostname()
    osinfo['plat'] = platform
    osinfo['cpu'] = cpuinfo.get_cpu_info()
    osinfo['mem'] = psutil.virtual_memory()
    osinfo['net'] = psutil.net_if_addrs()
 
    return render_template('info.html', info = osinfo)
+
+@app.route('/monitor')
+def monitor():
+   return render_template('monitor.html')
+
+apidata = {}
+
+olddata = {}
+olddata['disk_write'] = 0
+olddata['disk_read'] = 0
+olddata['net_sent'] = 0
+olddata['net_recv'] = 0
+@app.route('/api/monitor')
+def api_monitor():
+   global net_sent_o 
+   apidata['cpu'] = psutil.cpu_percent(interval=0.9)
+   apidata['mem'] = psutil.virtual_memory().percent
+   apidata['disk'] = psutil.disk_usage('/').percent
+   diskio = psutil.disk_io_counters()
+   netio = psutil.net_io_counters()
+
+   apidata['net_sent'] = 0 if olddata['net_sent'] == 0 else netio.bytes_sent - olddata['net_sent']
+   olddata['net_sent'] = netio.bytes_sent
+   apidata['net_recv'] = 0 if olddata['net_recv'] == 0 else netio.bytes_recv - olddata['net_recv']
+   olddata['net_recv'] = netio.bytes_recv
+
+   apidata['disk_write'] = 0 if olddata['disk_write'] == 0 else diskio.write_bytes - olddata['disk_write']
+   olddata['disk_write'] = diskio.write_bytes
+   apidata['disk_read'] = 0 if olddata['disk_read'] == 0 else diskio.read_bytes - olddata['disk_read']
+   olddata['disk_read'] = diskio.read_bytes
+
+   return jsonify(apidata)
